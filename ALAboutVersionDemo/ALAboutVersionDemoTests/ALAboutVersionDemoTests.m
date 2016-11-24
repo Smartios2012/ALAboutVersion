fa467baa32e654f2c9f3d0da205b4ca61ffd8bbd
DHF <dhf@jishubu-huangjialiang.local>
2016-11-24 15:47:57 +0800

    提交代码

---
+++ ALAboutVersionDemo/ALAboutVersionDemoTests/ALAboutVersionDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  ALAboutVersionDemoTests.m
+//  ALAboutVersionDemoTests
+//
+//  Created by DHF on 2016/11/24.
+//  Copyright © 2016年 MZ. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ALAboutVersionDemoTests : XCTestCase
+
+@end
+
+@implementation ALAboutVersionDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
